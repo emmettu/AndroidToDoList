ff247ae708e0e33c5cba15695b48a9db5dca72f3
Emmett Underhill <emmettu@gmail.com>
2014-09-26 21:29:45 -0600

    Got rid of some warnings from extra imports

--- src/com/example/todolist/ToDoArchive.java
+++ src/com/example/todolist/ToDoArchive.java
@@ -25,8 +25,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
 
 public class ToDoArchive extends Activity {
